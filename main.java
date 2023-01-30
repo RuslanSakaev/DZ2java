d26a2124f433d1695247ff3f771b8459399f3906
RuslanSakaev <hardist@mail.ru>
2023-01-30 19:57:54 +0300

    добавил реализацию алгоритма

--- main.java
+++ main.java
@@ -12,3 +12,38 @@
 // Результат нужно сохранить в файле output.txt
 // 1000
 
+
+// +Реализовать функцию возведения числа а в степень b. a, b ∈ Z. 
+
+// Сводя количество выполняемых действий к минимуму. 
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("\nВведите число: ");
+        int a = iScanner.nextInt();
+        System.out.print("Введите степень числа: ");
+        int b = iScanner.nextInt();
+        iScanner.close();
+
+        if (b == 0) {
+            System.out.println("Число " + a + " в нулевой степени = 1");
+        } else if (b == 1) {
+            System.out.println("Число " + a + " ^ " + b + " = " + a + " ");
+        } else {
+            double c = a;
+            for (int i = 1; i < Math.abs(b); i++) {
+                c = c * a;
+            }
+            if (0 < b) {
+                System.out.println("Число " + a + " ^ " + b + " = " + c + " ");
+            }
+            if (b < 0) {
+                System.out.println("Число " + a + " ^ " + b + " = " + 1 / c + " ");
+            }
+        }
+
+    }
+}
